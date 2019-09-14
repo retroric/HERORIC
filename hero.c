071476e83aa11776f41b7c0a732370e0454d55e9
Laurent Den <laurentd75@yahoo.fr>
2019-09-14 04:34:53 +0200

    v0.2 - added comments, VSCode project files, and Original maps and draft graphics elements/tiles

--- hero.c
+++ hero.c
@@ -1,9 +1,10 @@
 //
-// Rodoric H.E.R.O.
+// Rodoric H.E.R.O. - Animation test
 //
 // Based on Roderick H.E.R.O. for Atari VCS 2600
 //
-// (c) The Fox 2015
+// @author retroric <retroric48k@yahoo.fr>  
+// @year 2015
 //
 
 #include <lib.h>
@@ -12,6 +13,8 @@
 // VARIABLES (MUST BE DECLARED FIRSTHAND, OR USE FORWARD DECLARATIONS)
 unsigned char HIRES_BLANK_SEXTET = 0x40;
 
+// All Sprites are 24 lines x 12 pixels and include an attribute byte for foreground colour 
+// => 24 lines x 3 bytes: first byte contains colour attribute, and the next 2 are the pixels (6 pixels per byte)
 unsigned char SPRITE_Hero_Hovering_Right[][3] =  {
 	{ 0x03, 0x7F, 0x70 },
 	{ 0x03, 0x46, 0x40 },
@@ -66,6 +69,7 @@ unsigned char SPRITE_Hero_Hovering_Left[][3] =  {
 	{ 0x07, 0x40, 0x70 },
 };
 
+// ROTOR animation sprites are only one line high, and contain 3 frames
 unsigned char ANIM_Hero_Rotor_Right[][3] =  {
 	{ 0x03, 0x7F, 0x70 },
 	{ 0x03, 0x4F, 0x40 },
@@ -78,7 +82,7 @@ unsigned char ANIM_Hero_Rotor_Left[][3] =  {
 	{ 0x03, 0x40, 0x58 }
 };
 
-/* Start addresses of the vertical rows spanning 6 pixels: 33 rows (0..32), containing 40 cells (00..39) spanning 6 pixels */
+/* Start addresses of the vertical rows spanning 6 pixels in HIRES mode: 33 rows (0..32), containing 40 cells (00..39) spanning 6 pixels */
 int row_start_addr[] = { 0xA000, 0xA0F0, 0xA1E0, 0xA2D0, 0xA3C0, 0xA4B0, 0xA5A0, 0xA690, 0xA780, 0xA870, 
 					     0xA960, 0xAA50, 0xAB40, 0xAC30, 0xAD20, 0xAE10, 0xAF00, 0xAFF0, 0xB0E0, 0xB1D0, 
 					     0xB2C0, 0xB3B0, 0xB4A0, 0xB590, 0xB680, 0xB770, 0xB860, 0xB950, 0xBA40, 0xBB30, 
@@ -99,7 +103,7 @@ void play_hero();
 
 // Declare the assembly code functions
 //void cli(); // enable interrupts
-//void sei(); // disabled interrupts
+//void sei(); // disable interrupts
 
 // ============ MAIN ==============
 
@@ -110,7 +114,7 @@ void play_hero();
 #define KEYB_SPECIAL_KEYS_LINE_COL_PATTERN	0x209		// Idem pour touches de contrôle (Shift, Ctrl, Funct)
 #define KEYB_NORM_KEYS_COL_PATTERN			0x20A		// Motif colonne dernière touche pressée	
 #define KEYB_NORM_KEYS_PATTERN_FOR_COL4		0x20B		// Idem pour touches de contrôle (Shift, Ctrl, Funct)
-#define is_shifted() ( ((unsigned char) *0x20C) & 128)	// #FF = Majuscule pressée, #7F = minuscule pressée
+#define is_shifted() ( ((unsigned char) *0x20C) & 128)	// #FF = Majuscule pressée, #7F = minuscule pressée (bit 7 set or cleared)
 #define KEYB_REPEAT_COUNTER					0x20E		// Compteur de répétition clavier
 
 
@@ -130,6 +134,16 @@ void play_hero();
 #define NO_KEY 0x38 // NO KEY PRESSED
 
 
+#define XDIR_RIGHT  1   // Going RIGHT (doubles as increment value for going right)
+#define XDIR_LEFT  -1   // Going LEFT  (doubles as increment value for going left)
+#define XMIN 0          // Min X starting cell for Sprite = 0
+#define XMAX 37	        // Max X starting cell for Srite = 37 (sprite will spawn cells 37..39)
+#define YMIN 0          // Min Y starting cell for Sprite = 0
+#define YMAX 29	        // Max Y starting cell for Srite = 29 (sprite will spawn cells 29..32)
+
+#define ROTOR_ANIM_FRAMES 3 // Animation frames for H.E.R.O. backpack rotor
+
+
 void main() {
 	//test_keys();
 	play_hero();
@@ -144,26 +158,17 @@ void test_keys() {
  unsigned char key      = NO_KEY; // 0x38 = NO KEY PRESSED
  unsigned char prev_key = NO_KEY; // 0x38 = NO KEY PRESSED
  
- for(; key != KEY_SPACE;) {
+ for(; key != KEY_SPACE;) { // loop until SPACE is pressed to quit
 	key = *keyb_norm_key_press_addr;
 	if(key != NO_KEY) {	// 0x38 = NO KEY PRESSED
 		if(key != prev_key) {
+			// Do not log repeat key presses
 			printf("Key pressed: %d\n", key);
 			prev_key = key;
 		}
 	}
   }  
 }
-
-
-#define XDIR_RIGHT  1   // Going RIGHT (doubles as increment value for going right)
-#define XDIR_LEFT  -1   // Going LEFT  (doubles as increment value for going left)
-# define XMIN 0         // Min X starting cell for Sprite = 0
-#define  XMAX 37	    // Max X starting cell for Srite = 37 (sprite will spawn cells 37..39)
-# define YMIN 0         // Min Y starting cell for Sprite = 0
-#define  YMAX 29	    // Max Y starting cell for Srite = 29 (sprite will spawn cells 29..32)
-
-#define ROTOR_ANIM_FRAMES 3 // Animation frames for H.E.R.O. backpack rotor
 
 // =======================================================
 // Play HERO using arrow keys, press SPACE to end game
@@ -191,7 +196,8 @@ void play_hero() {
 				erase_1x4cell(x-1, y);           // ERASE SPRITE'S OLD POSITION: We ONLY need to erase the FIRST PREVIOUS column of the sprite
 				//draw_hero_hovering_rotor(x, y, xdir, rotor_anim_frame); // LAST: REDRAW rotor with current animation frame
 			}
-			else { // We were previously going LEFT  => Just REVERSE current direction and redraw new sprite
+			else { 	// EITHER We were previously going LEFT  => Just REVERSE current direction and redraw new sprite
+					// OR we reached maximum X position, so we stay at current position, still facing right (and redrawing the sprite is not really necessary)
 				xdir = XDIR_RIGHT;
 				draw_hero_hovering_right(x, y, 1);
 			}
@@ -204,7 +210,8 @@ void play_hero() {
 				draw_hero_hovering_left(x, y, 1);	// REDRAW SPRITE at new position
 				//draw_hero_hovering_rotor(x, y, xdir, rotor_anim_frame); // LAST: REDRAW rotor with current animation frame
 			}
-			else { // We were previously going RIGHT => Just REVERSE current direction and redraw new sprite
+			else { 	// EITHER we were previously going RIGHT => Just REVERSE current direction and redraw new sprite
+					// OR we reached minimum X position, so we stay at current position, still facing left (and redrawing the sprite is not really necessary)
 				xdir = XDIR_LEFT;
 				draw_hero_hovering_left(x, y, 1);
 			}
@@ -327,6 +334,7 @@ void erase_1x4cell(char xstart, char ystart) {
 		curr_addr += 40; /* offset for start of xcell on next line */
 	}
 }
+
 // ======================================================================
 // Erase a 3x4 group of cells (18x24 pixels)
 // ======================================================================
@@ -354,6 +362,3 @@ void erase_3x1cell(char xstart, char ystart) {
 		curr_addr += 38; /* 38 = 40-2 (already incremented pointer twice): offset for start of xcell on next line */
 	}
 }
-
-
-
